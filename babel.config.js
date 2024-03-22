1cfac6f98baccc2b2ffb5140085a0dfb88a129af
wsilvadev <wsilvadev@gmail.com>
2024-03-21 21:04:24 -0300

    feat: create folders and configure babel

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,26 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["./src"],
+          extensions: [".js", ".jsx", ".ts", ".tsx"],
+          alias: {
+            "@src": "./src",
+            "@assets": "./src/assets",
+            "@components": "./src/components",
+            "@config": "./src/config",
+            "@hooks": "./src/hooks",
+            "@routes": "./src/routes",
+            "@screens": "./src/screens",
+            "@services": "./src/services",
+            "@utils": "./src/utils",
+          },
+        },
+      ],
+    ],
   };
 };
